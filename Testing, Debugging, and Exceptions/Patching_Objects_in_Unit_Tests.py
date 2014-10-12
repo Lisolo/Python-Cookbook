af88a94ffc8fb55cb42324c56d61bc21320993bc
Solo <iamsoloa@gmail.com>
2014-10-12 19:45:03 +0800

    Update Patching_Objects_in_Unit_Tests.py

--- Testing, Debugging, and Exceptions/Patching_Objects_in_Unit_Tests.py
+++ Testing, Debugging, and Exceptions/Patching_Objects_in_Unit_Tests.py
@@ -132,7 +132,7 @@ def dowprices():
     u = urlopen('http://finance.yahoo.com/d/quotes.csv?s=@^DJI&f=sl1')
     lines = (line.decode('utf-8') for line in u)
     rows = (row for row in csv.reader(lines) if len(row) == 2)
-    prices = { name.float(price) for name, price in rows }
+    prices = { name:float(price) for name, price in rows }
     return prices
 """
 
@@ -148,7 +148,7 @@ import example
 
 sample_data = io.BytesIO(b'''\
 "IBM", 91.1\r
-""AA", 13.25\r
+"AA", 13.25\r
 "MSFT", 27.72\r
 \r
 ''')
