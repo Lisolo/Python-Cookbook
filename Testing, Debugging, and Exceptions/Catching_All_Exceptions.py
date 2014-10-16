5b55201f0d33cfb3fcf749e66475f94476a9f6f1
Solo <iamsoloa@gmail.com>
2014-10-16 09:56:17 +0800

    Create Catching_All_Exceptions.py

---
+++ Testing, Debugging, and Exceptions/Catching_All_Exceptions.py
@@ -0,0 +1,72 @@
+# coding=utf-8
+
+"""
+Problem
+
+You want to write code that catches all exceptions.
+
+Solution
+
+To catch all exceptions, write an exception handler for Exception, 
+as shown here:
+
+try:
+    ...
+except Exception as e:
+    ...
+    log('Reason:', e)    # Important!
+
+This will catch all exceptions save SystemExit, KeyboardInterrupt, and GeneratorExit. 
+If you also want to catch those exceptions, change Exception to BaseException.
+"""
+
+"""
+Discussion
+
+Catching all exceptions is sometimes used as a crutch by programmers 
+who can’t remember all of the possible exceptions that might occur in complicated operations. 
+As such, it is also a very good way to write undebuggable code if you are not careful.
+
+Because of this, if you choose to catch all exceptions, 
+it is absolutely critical to log or report the actual reason for the exception somewhere 
+(e.g., log file, error message printed to screen, etc.). 
+If you don’t do this, your head will likely explode at some point. 
+Consider this example:
+"""
+def parse_int(s):
+    try:
+        n = int(v)
+    except Exception:
+        print("Couldn't parse")
+
+"""
+If you try this function, it behaves like this:
+
+>>> parse_int('n/a')
+Couldn't parse
+>>> parse_int('42')
+Couldn't parse
+>>>
+"""
+def parse_int2(s):
+    try:
+        n = int(v)
+    except Exception as e:
+        print("Couldn't parse")
+        print("Reason:", e)
+
+"""
+In this case, you get the following output, 
+which indicates that a programming mistake has been made:
+
+>>> parse_int('42')
+Couldn't parse
+Reason: name 'v' is not defined
+>>>
+"""
+
+"""
+All things being equal, it’s probably better to be as precise as possible in your exception handling. 
+However, if you must catch all exceptions, 
+just make sure you give good diagnostic information or propagate the exception so that cause doesn’t get lost.
+"""
